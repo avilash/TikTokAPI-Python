117b9bb39753447409d6abd8205877ee040b51ba
avilash <avilashkumar4@gmail.com>
2020-06-29 04:23:08 +0530

    Adding Github Workflow

--- setup.py
+++ setup.py
@@ -5,26 +5,28 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TikTokAPI-Python",
+    packages=setuptools.find_packages(),
     version="0.0.1",
     license='MIT',
     author="Avilash Kumar",
-    author_email="avilash.k@greyorange.sg",
+    author_email="avilashkumar4@gmail.com",
     description="Unofficial TikTok API wrapper in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/avilash/TikTokAPI",
     download_url='https://github.com/avilash/TikTokAPI/tarball/master',
-    keywords=['tiktok', 'python3', 'api', 'unofficial', 'tiktok-api', 'tiktok api'],
+    keywords=['tiktok', 'python', 'api', 'tiktok-api', 'tiktok api'],
     install_requires=[
         'requests',
         'pyppeteer',
         "pyppeteer_stealth"
     ],
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
     ],
     include_package_data=True
 )
