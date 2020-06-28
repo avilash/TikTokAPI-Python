4b63902c782b92c9782944a8e4991765f13b05a6
avilash <avilashkumar4@gmail.com>
2020-06-29 04:26:12 +0530

    Package name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TikTokAPI-Python",
+    name="PyTikTokAPI",
     packages=setuptools.find_packages(),
     version="0.0.1",
     license='MIT',
